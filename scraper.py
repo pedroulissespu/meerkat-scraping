bf37bf679764b97043479bcf110c8cdeefef4809
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2024-06-07 15:27:36 -0300

    feat: adding new fields

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import json
 
+from selenium.webdriver.common.by import By
+
 
 def scraping():
     chrome_options = Options()
@@ -16,14 +18,45 @@ def scraping():
 
     driver.get("http://bianca.com")
 
-    # Extract content (example: title of the page)
     title = driver.title
+    cookies = driver.get_cookies()
+    text = driver.find_element(By.XPATH, '/html').text
+    inner_html = driver.find_element(By.XPATH, '/html').get_attribute('innerHTML')
+    is_selected = driver.find_element(By.XPATH, '/html').is_selected()
+    css_value = driver.find_element(By.XPATH, '/html').value_of_css_property('propriedade_css')
+
+    attributes_rows = []
+
+    rows = driver.find_elements(By.XPATH, 'xpath_das_linhas')
+    for row in rows:
+        cells = row.find_elements(By.TAG_NAME, 'td')
+        attributes_rows.append(cells)
+
+    link = driver.find_element(By.XPATH, '/html').get_attribute('h1')
+
+    items = driver.find_elements(By.XPATH, '/html')
+
+    list_items = []
+    for item in items:
+        list_items.append(item.text)
+
+    data = [
+        {'title': title},
+        {'cookies': cookies},
+        {'text_html': text},
+        {'inner_html': inner_html},
+        {'is_selected': is_selected},
+        {'css_value': css_value},
+        {'attributes_rows': attributes_rows},
+        {'link': link},
+        {'list_items': list_items}
+    ]
 
     driver.quit()
 
     return {
         'statusCode': 200,
-        'body': json.dumps({'title': title})
+        'body': json.dumps(data)
     }
 
 
